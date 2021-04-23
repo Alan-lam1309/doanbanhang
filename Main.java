2f25bdf52143c0a0fa9bd76f9c9eafe36ee34bed
phudeptrai1309 <82869865+phudeptrai1309@users.noreply.github.com>
2021-04-23 10:01:30 +0700

    main
    class a
    class

--- Main.java
+++ Main.java
@@ -19,8 +19,16 @@ public class Main {
      */
     public static void main(String[] args) throws ClassNotFoundException  {
       //Dangnhap a= new Dangnhap();
-       new trangchu();
+      // new trangchu();
+      //////////////////////////////////////////////
       // new doanhthutong();
+      
+      
+      
+      
+      
+      
+      
     }
 }
 
