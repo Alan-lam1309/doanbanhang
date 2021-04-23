dbe0ce570ad050481275757a5121e86b3f7f9b4b
phudeptrai1309 <82869865+phudeptrai1309@users.noreply.github.com>
2021-04-23 08:33:53 +0700

    newwwwww lineeeee
    create :DDDDDĐ

---
+++ doanhthutong.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doanbanhang;
+import javax.swing.*;
+
+/**
+ *
+ * @author Quoc Phu
+ */
+public class doanhthutong extends JFrame{
+
+    public doanhthutong() {
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setVisible(true);
+        setLayout(null);
+        setResizable(false);
+        
+        JPanel p=new JPanel();
+        JLabel Doanhthutonglb=new JLabel();
+        
+        
+    }
+    
+}
